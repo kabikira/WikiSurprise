8842d6056bf846652952aaaf5398578b10d53734
kabikira <kabikirar2@protonmail.com>
2023-06-05 21:46:32 +0900

    "ArticleTableViewCellにprepareForReuseメソッド追加#13"

--- WikiSurprise/Article/View/ArticleTableViewCell.swift
+++ WikiSurprise/Article/View/ArticleTableViewCell.swift
@@ -5,4 +5,16 @@
 //  Created by koala panda on 2023/06/04.
 //
 
-import Foundation
+import UIKit
+
+final class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet private weak var title: UILabel!
+
+    override func prepareForReuse() {
+        title.text = nil
+    }
+
+   
+
+}
+
