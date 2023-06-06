1a8c2450209998b68a331fc24f38427045fe6ff4
kabikira <kabikirar2@protonmail.com>
2023-06-06 12:37:54 +0900

    "スタティックプロパティclassName追加 #14"

--- WikiSurprise/History/View/HistoryTableViewCell.swift
+++ WikiSurprise/History/View/HistoryTableViewCell.swift
@@ -11,8 +11,13 @@ final class HistoryTableViewCell: UITableViewCell {
 
     @IBOutlet private weak var histryTitle: UILabel!
 
+    // ハードコーティングを防ぐため
+    static var className: String { String(describing: HistoryTableViewCell.self)}
+
     override func prepareForReuse() {
         super.prepareForReuse()
         histryTitle.text = nil
     }
+
+
 }
