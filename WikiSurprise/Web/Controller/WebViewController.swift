03879a0131048903d35a7b3046bbe63a58e4150c
kabikira <kabikirar2@protonmail.com>
2023-06-08 00:39:55 +0900

    WebViewControllerクラス実装 #5

--- WikiSurprise/Web/Controller/WebViewController.swift
+++ WikiSurprise/Web/Controller/WebViewController.swift
@@ -6,16 +6,26 @@
 //
 
 import UIKit
+import WebKit
 
-class WebViewController: UIViewController {
+final class WebViewController: UIViewController {
 
+    @IBOutlet private weak var webView: WKWebView!
+
+    private var wikiAritcle: Article?
+
+    func configure(wikiAritcle: Article) {
+        self.wikiAritcle = wikiAritcle
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        guard
+            let wikiAritcle = wikiAritcle,
+            let url = URL(string: wikiAritcle.urlStr) else {
+            return
+        }
+        webView.load(URLRequest(url: url))
     }
-    
-
 
 
 }
