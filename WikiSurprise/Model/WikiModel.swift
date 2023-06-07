7639b65def233ee5a610276097b86320e7e9f794
kabikira <kabikirar2@protonmail.com>
2023-06-08 00:23:59 +0900

    WikiModelにurlStr追加 #36

--- WikiSurprise/Model/WikiModel.swift
+++ WikiSurprise/Model/WikiModel.swift
@@ -10,6 +10,7 @@ import Foundation
 struct Article: Codable {
     let id: Int
     let title: String
+    var urlStr: String { "https://ja.wikipedia.org/wiki/\(title)".addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""}
 }
 
 struct Query: Codable {
