6a830f310cb6005ee838b94e94daffc2b473ad88
kabikira <kabikirar2@protonmail.com>
2023-06-11 00:04:54 +0900

    fix WikiAPI修正 #6

--- WikiSurprise/Utility/API/WikiAPI.swift
+++ WikiSurprise/Utility/API/WikiAPI.swift
@@ -35,4 +35,29 @@ import Foundation
 //    }
 //}
 
+public final class WikiAPI {
+    public struct GetArticles: WikiRequest {
 
+        public typealias Response = [Article]
+
+        public var path: String {
+            return "/w/api.php"
+        }
+
+        public var method: HTTPMethod {
+            return .get
+        }
+
+        public var queryItems: [URLQueryItem] {
+            return [
+                URLQueryItem(name: "action", value: "query"),
+                URLQueryItem(name: "list", value: "random"),
+                URLQueryItem(name: "rnnamespace", value: "0"),
+                URLQueryItem(name: "rnlimit", value: "10"),
+                URLQueryItem(name: "format", value: "json")
+            ]
+        }
+
+
+    }
+}
