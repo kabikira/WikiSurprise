cb62bc8ba680128a1937547b3c99817ee695a763
kabikira <kabikirar2@protonmail.com>
2023-06-11 01:32:19 +0900

    add HTTPClient.swift追加 #6

---
+++ WikiSurprise/Utility/API/HTTPClient.swift
@@ -0,0 +1,28 @@
+//
+//  HTTPClient.swift
+//  WikiSurprise
+//
+//  Created by koala panda on 2023/06/11.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func sendRequest(_ urlRequest: URLRequest, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void)
+}
+
+extension URLSession: HTTPClient {
+    public func sendRequest(_ urlRequest: URLRequest, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
+        let task = dataTask(with: urlRequest) { data, urlResponse, error in
+            switch (data, urlResponse, error) {
+            case (_, _, let error?):
+                completion(Result.failure(error))
+            case (let data?, let urlResponse as HTTPURLResponse, _):
+                completion(Result.success((data, urlResponse)))
+            default:
+                fatalError("invalid response combination \(String(describing: (data, urlRequest, error))).")
+            }
+        }
+        task.resume()
+    }
+}
