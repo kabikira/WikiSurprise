9c2d66df7dcecd458fc0a9231c890c37983c2a73
kabikira <kabikirar2@protonmail.com>
2023-06-10 18:03:16 +0900

    add HTTPMethod.swift追加 #6

--- WikiSurprise/Utility/API/Request/HTTPMethod.swift
+++ WikiSurprise/Utility/API/Request/HTTPMethod.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case head = "HEAD"
+    case delete = "DELETE"
+    case patch = "PATCH"
+    case trace = "TRACE"
+    case options = "OPTIONS"
+    case connect = "CONNECT"
+}
--- WikiSurprise/Utility/API/Request/WikiRequest.swift
+++ WikiSurprise/Utility/API/Request/WikiRequest.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+public protocol WikiRequest {
+    associatedtype Response: Decodable
+    var baseURL: URL { get }
+    // パス"/w/api.php"
+    var path: String { get }
+    var method: HTTPMethod { get }
+    // クエリ "?action=query&list=random&rnnamespace=0&rnlimit=10&format=json"
+    var queryItems: [URLQueryItem] { get }
+}
+
+public extension WikiRequest {
+    var baseURL: URL {
+        return URL(string: "https://ja.wikipedia.org")!
+    }
+}
