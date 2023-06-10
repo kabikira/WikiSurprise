5e8f5272f0c2568e1b33a6f1778936d0a6701ed7
kabikira <kabikirar2@protonmail.com>
2023-06-10 18:02:43 +0900

    add HTTPMethod.swift追加 #6

---
+++ WikiSurprise/Utility/API/Request/HTTPMethod.swift
@@ -0,0 +1,8 @@
+//
+//  HTTPMethod.swift
+//  WikiSurprise
+//
+//  Created by koala panda on 2023/06/10.
+//
+
+import Foundation
---
+++ WikiSurprise/Utility/API/Request/WikiRequest.swift
@@ -0,0 +1,8 @@
+//
+//  WikiRequest.swift
+//  WikiSurprise
+//
+//  Created by koala panda on 2023/06/10.
+//
+
+import Foundation
