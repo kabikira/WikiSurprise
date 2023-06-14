67122f501718ef2a73b8e0251882f4ef2c69a215
kabikira <kabikirar2@protonmail.com>
2023-06-14 12:44:31 +0900

    add intoreductionImageniにダミーの画像追加 #29

--- WikiSurprise/AppIntroduction/Controller/AppIntroductionViewController.swift
+++ WikiSurprise/AppIntroduction/Controller/AppIntroductionViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class AppIntroductionViewController: UIViewController {
 
+    private let testImage: UIImage! = UIImage(named: "test")
+
     @IBOutlet weak var intorductionImage: UIImageView! {
+        didSet {
+            intorductionImage.image = testImage
+
+        }
     }
     @IBOutlet weak var doneButton: UIButton!
 
