c727d62f2b6926620c21ae197904b45b32370d05
kabikira <kabikirar2@protonmail.com>
2024-08-13 00:12:56 +0900

    テストコード実装

--- WikiSurpriseTests/WikiSurpriseTests.swift
+++ WikiSurpriseTests/WikiSurpriseTests.swift
@@ -52,30 +52,95 @@ final class WikiAPIErrorTests: XCTestCase {
         }
 }
 
+final class WikiClientTests: XCTestCase {
+    var httpClient: StubHTTPClient!
+    var wikiClient: WikiClient!
 
-final class WikiSurpriseTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        httpClient = StubHTTPClient()
+        wikiClient = WikiClient(httpClient: httpClient)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testSendRequestSuccess() async throws {
+        let json = WikiResponse.exampleJSON
+        httpClient.data = Data(json.utf8)
+        httpClient.response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                              statusCode: 200,
+                                              httpVersion: nil,
+                                              headerFields: nil)!
+        let request = WikiAPI.GetArticles()
+        do {
+            let articles = try await wikiClient.send(request: request).query.random ?? []
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+            XCTAssertEqual(articles[0].id, 3216088)
+            XCTAssertEqual(articles[0].title, "ATMOS 2000")
+            XCTAssertEqual(articles[0].urlStr, "https://ja.wikipedia.org/wiki/ATMOS%202000")
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+        } catch {
+            XCTFail("Expected URLError, but got \(error)")
         }
     }
 
+    // 通信に失敗した場合
+    func testFailureByConnectionError() async throws {
+        httpClient.error = URLError(.cannotConnectToHost)
+        let json = WikiResponse.exampleJSON
+        httpClient.data = Data(json.utf8)
+        httpClient.response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                              statusCode: 200,
+                                              httpVersion: nil,
+                                              headerFields: nil)!
+        let request = WikiAPI.GetArticles()
+
+        do {
+            _ = try await wikiClient.send(request: request).query.random ?? []
+            XCTFail("Expected to throw, but succeeded")
+        } catch let WikiClientError.connectionError(error as URLError) {
+            print("エラー\(error)")
+
+            XCTAssertEqual(error.code, .cannotConnectToHost)
+        } catch {
+            XCTFail("Expected WikiClientError.connectionError, but got \(error)")
+        }
+    }
+
+    // レスポンスの解釈に失敗した場合
+    func testFailureByResponseParseError() async throws {
+        let json = "{}"
+        httpClient.data = Data(json.utf8)
+        httpClient.response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                              statusCode: 200,
+                                              httpVersion: nil,
+                                              headerFields: nil)!
+        let request = WikiAPI.GetArticles()
+        do {
+            _ = try await wikiClient.send(request: request).query.random ?? []
+            XCTFail("Expected to throw, but succeeded")
+        } catch {
+            return
+        }
+    }
+
+    // エラーレスポンスを受け取った場合
+    func testFailureByAPIError() async throws {
+        let json = WikiAPIError.exampleJSON
+        httpClient.data = Data(json.utf8)
+        httpClient.response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                              statusCode: 400,
+                                              httpVersion: nil,
+                                              headerFields: nil)!
+        let request = WikiAPI.GetArticles()
+        do {
+            let articles = try await wikiClient.send(request: request).query.random ?? []
+            XCTFail("Expected to throw, but succeeded")
+
+        } catch let WikiClientError.responseParseError(apiError as WikiAPIError) {
+            XCTAssertEqual(apiError.code, "badvalue")
+            XCTAssertEqual(apiError.info, "Unrecognized value for parameter \"action\": .")
+            XCTAssertEqual(apiError.documentationURI, "See https://ja.wikipedia.org/w/api.php for API usage. Subscribe to the mediawiki-api-announce mailing list at <https://lists.wikimedia.org/postorius/lists/mediawiki-api-announce.lists.wikimedia.org/> for notice of API deprecations and breaking changes.")
+        } catch {
+            XCTFail("Expected WikiClientError.responseParseError with WikiAPIError, but got \(error)")
+        }
+    }
 }
